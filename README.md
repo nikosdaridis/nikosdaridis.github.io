b97e4e776d3b5b671115adec453056db87494ad4
NikosDaridis <101875554+NikosDaridis@users.noreply.github.com>
2023-07-13 18:43:52 +0300

    react

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Responsive portfolio website
 
-
 <a href="https://nikosdaridis.github.io/"><img src ="https://github.com/nikosdaridis/nikosdaridis.github.io/raw/main/images/Readme.png" /></a>
 
 This is my website to introduce myself, show my skills, portfolio and a way to contact me.
--- script.js
+++ script.js
@@ -1,75 +1,78 @@
-const lightModeIcon = document.querySelector('#light-mode-icon');
-const button = document.getElementById("mailSendButton");
-const menuIcon = document.querySelector('#menu-icon');
-const navbar = document.querySelector('.navbar');
-const sections = document.querySelectorAll('section');
-const navLinks = document.querySelectorAll('header nav a');
-const header = document.querySelector('header');
-const contactForm = document.getElementById("contact-form");
+const lightModeIcon = document.querySelector("#light-mode-icon");
+const button = document.querySelector("#mailSendButton");
+const menuIcon = document.querySelector("#menu-icon");
+const navbar = document.querySelector(".navbar");
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("header nav a");
+const header = document.querySelector("header");
+const contactForm = document.querySelector("#contact-form");
 var darkTheme = false;
 
 function ToggleLightDarkMode() {
-    darkTheme = !darkTheme;;
-    lightModeIcon.classList.toggle('bxs-sun');
-    document.body.classList.toggle('dark-theme');
+  darkTheme = !darkTheme;
+  lightModeIcon.classList.toggle("bxs-sun");
+  document.body.classList.toggle("dark-theme");
 }
 
 function ToggleMenuIcon() {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
 }
 
 function ScrollActiveHighlight() {
-    sections.forEach(section => {
-        let top = window.scrollY;
-        let offset = section.offsetTop - 150;
-        let height = section.offsetHeight;
-        let id = section.getAttribute('id');
+  sections.forEach((section) => {
+    let top = window.scrollY;
+    let offset = section.offsetTop - 150;
+    let height = section.offsetHeight;
+    let id = section.getAttribute("id");
 
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-        };
-    });
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
 
-    header.classList.toggle('navbarLine', window.scrollY > 200);
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
-};
+  header.classList.toggle("navbarLine", window.scrollY > 200);
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+}
 
-const typed = new Typed('.multiple-text', {
-    strings: ['Software Engineer', 'Game Developer'],
-    typeSpeed: 100,
-    backSpeed: 50,
-    backDelay: 2000,
-    loop: true
+const typed = new Typed(".multiple-text", {
+  strings: ["Software Engineer", "Game Developer"],
+  typeSpeed: 100,
+  backSpeed: 50,
+  backDelay: 2000,
+  loop: true,
 });
 
 function SendMail() {
-    let params = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        message: document.getElementById("message").value,
-    }
+  let params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
 
-    emailjs.send("service_csks0d8", "template_l4aoenf", params)
-        .then(function (response) {
-            if (response.status == 200) {
-                button.innerText = "Message Sent Successfully";
-                setTimeout(() => button.innerText = "Send Message", 5000);
-            } else {
-                button.innerText = "Something Went Wrong, Try Again";
-                setTimeout(() => button.innerText = "Send Message", 5000);
-            }
-        })
+  emailjs
+    .send("service_csks0d8", "template_l4aoenf", params)
+    .then(function (response) {
+      if (response.status == 200) {
+        button.innerText = "Message sent successfully";
+        setTimeout(() => (button.textContent = "Send Message"), 5000);
+      } else {
+        button.innerText = "There was an error. Try again later";
+        setTimeout(() => (button.textContent = "Send Message"), 5000);
+      }
+    });
 }
 
 lightModeIcon.addEventListener("click", ToggleLightDarkMode);
 menuIcon.addEventListener("click", ToggleMenuIcon);
 document.addEventListener("scroll", ScrollActiveHighlight);
 contactForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    SendMail();
+  event.preventDefault();
+  SendMail();
 });
